f8b566e8bb7258abc82161980b83e4ef8535a93f
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-16 12:56:02 -0400

    showPoorest as well as file formatting

--- src/littlepeoplemaxmin/LittlePeopleMaxMin.java
+++ src/littlepeoplemaxmin/LittlePeopleMaxMin.java
@@ -19,7 +19,7 @@ public class LittlePeopleMaxMin {
 
     public static void main(String[] args) {
 
-        //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - "INPUT" DATA
+        //"INPUT" DATA
         // Hardcoded (vs. read from a file) to simplify this for testing
         String[] name = {"Happy", "Grumpy", "Doc", "Sneezy", "Sleepy",
                 "Bashful", "Dopey"};
@@ -27,7 +27,7 @@ public class LittlePeopleMaxMin {
         int[] age = {55, 65, 80, 40, 45, 40, 50};
         double[] salary = {50000.00, 35000.00, 100000.00, 54321.99, 9123.45,
                 64000.88, 10000.01};
-        //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - PROCDESSING
+        //PROCESSING
         showRawData(name, pic, age, salary);
 
         char choice = getUserOption();
@@ -36,7 +36,7 @@ public class LittlePeopleMaxMin {
 
         System.exit(0);
     }
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     private static char getUserOption() {
         String menu = "CHOOSE OPTION:\n"
                 + "P:  poorest\n"
@@ -49,7 +49,7 @@ public class LittlePeopleMaxMin {
         char choice = userResponse.charAt(0);
         return choice;
     }
-    //-------------------------------------------------------------------------
+
     private static void showRawData(String[] name, String[] pic, int[] age,
                                     double[] salary) {
         System.out.println("FYI:  THE DATA");
@@ -58,30 +58,27 @@ public class LittlePeopleMaxMin {
                     i, name[i], pic[i], age[i], salary[i]);
         System.out.println("\n");
     }
-    //-------------------------------------------------------------------------
-    private static void handleUserRequest(char choice,
-                                          String[] name, String[] pic, int[] age, double[] salary) {
 
-        // >>>>>>>  TO DO:  FILL IN PARAMETERS IN THESE CALLS
+    private static void handleUserRequest(char choice, String[] name, String[] pic, int[] age, double[] salary) {
         // IMPORTANT NOTE:  output includes selected person's name and pic
 
         switch (choice) {
             case 'P':
             case 'p':
-                showPoorest();
+                showPoorest(name,pic,salary);
                 break;
             case 'R':
             case 'r':
-                showRichest();
+                showRichest(name,pic,salary);
                 break;
             case 'O':
             case 'o':
             case '0':                                     // zero, just in case
-                showOldest();
+                showOldest(name,pic,age);
                 break;
             case 'Y':
             case 'y':
-                showYoungest();
+                showYoungest(name,pic,age);
                 break;
             case 'F':
             case 'f':
@@ -96,32 +93,31 @@ public class LittlePeopleMaxMin {
                 System.out.println("\nERROR:  option was invalid");
         }
     }
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-// >>>>>>>  TO DO:  fill in parameters below & write method bodies
-
-    //*************************************************************************
-    private static void showPoorest() {
+    private static void showPoorest(String name[],String pic[],double salary[]) {
+        double minSal=Integer.MAX_VALUE;
+        int spot=0;
+        for (int i=0; i<salary.length;i++){
+            if (salary[i]<minSal){
+                minSal=salary[i];
+                spot=i;
+            }
+        }
+        System.out.println("Name: "+name[spot]+ " "+pic[spot]);
+    }
+    private static void showRichest(String name[],String pic[],double salary[]) {
 
     }
-    //*************************************************************************
-    private static void showRichest() {
+    private static void showOldest(String name[],String pic[],int age[]) {
 
     }
-    //*************************************************************************
-    private static void showOldest() {
+    private static void showYoungest(String name[],String pic[],int age[]) {
 
     }
-    //*************************************************************************
-    private static void showYoungest() {
-
-    }
-    //*************************************************************************
     // First alphabetically, by name (not the first name in the array)
     private static void showFirst() {
 
     }
-    //*************************************************************************
     // Last alphabetically, by name (not the last name in the array)
     private static void showLast() {
 
